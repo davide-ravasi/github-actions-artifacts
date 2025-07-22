54ef1d0e1767109949c828c0ccc48cb09ed29870
Davide Ravasi <dravasi@d-edge.com>
2025-07-22 11:01:07 +0200

    feat: added inputs to js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,14 @@ const exec = require("@actions/exec");
 const github = require("@actions/github");
 
 function run() {
+  const bucketName = core.getInput("bucket");
+  const bucketRegion = core.getInput("bucket-region");
+  const distFolder = core.getInput("dist-folder");
+
   try {
+    core.info(`Deploying to S3 bucket: ${bucketName}`);
+    core.info(`Bucket region: ${bucketRegion}`);
+    core.info(`Distribution folder: ${distFolder}`);
     core.notice("Starting the deployment process...");
   } catch (error) {
     core.error(`Error: ${error}`);
