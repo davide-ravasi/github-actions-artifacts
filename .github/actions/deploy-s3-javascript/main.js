c4e91ee45333b8322000dd7b5fd83e426cdf9591
Davide Ravasi <dravasi@d-edge.com>
2025-07-28 08:47:15 +0200

    feat: add secrets

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,17 +1,29 @@
 const core = require("@actions/core");
-const exec = require("@actions/exec");
-const github = require("@actions/github");
+//const exec = require("@actions/exec");
+// const github = require("@actions/github"); // useful to communicate with GitHub API
 
 function run() {
-  const bucketName = core.getInput("bucket");
-  const bucketRegion = core.getInput("bucket-region");
-  const distFolder = core.getInput("dist-folder");
+  const bucketName = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+  const accessKeyId = core.getInput("access-key-id", { required: true });
+  const secretAccessKey = core.getInput("secret-access-key", {
+    required: true,
+  });
 
   try {
     core.info(`Deploying to S3 bucket: ${bucketName}`);
     core.info(`Bucket region: ${bucketRegion}`);
     core.info(`Distribution folder: ${distFolder}`);
+    core.info(`Access key ID: ${accessKeyId}`);
+    core.info(`Secret access key: ${secretAccessKey}`);
     core.notice("Starting the deployment process...");
+
+    // upload files to S3
+    // we can use the AWS CLI or SDK to upload files
+    // because is present in the GitHub Actions environment
+    // const s3UploadCommand = `aws s3 sync ${distFolder} s3://${bucketName} --region ${bucketRegion}`;
+    // exec.exec(s3UploadCommand);
   } catch (error) {
     core.error(`Error: ${error}`);
   }
